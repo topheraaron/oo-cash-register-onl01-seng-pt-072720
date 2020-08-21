b029af1bf5c15c172ec13fb4f3f12236b8d62334
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-21 23:02:32 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -6,4 +6,19 @@ class CashRegister
     @total = 0
     @discount = discount
   end
+
+  def add_item(title, price, quantity = 1)
+    if quantity > 1
+      i = 0
+      while i < quantity
+        @items << title
+        i += 1
+      end
+    else
+      @items << title
+    end
+    @total += price * quantity
+    @last_transaction_amount = @total
+    @total
+  end
 end
